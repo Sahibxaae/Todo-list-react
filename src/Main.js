b8563759c8f596ecaa7fb229943301b4c6d93a1a
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-11 17:32:36 +0530

    javascript added for task operation

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,51 @@
 import './Main.css'
-import React from 'react'
+import React, {useState} from 'react'
 
 const Main = () => {
-  return (
+    const [tasks,setTasks] = useState([]);
+    const [task,setTask] = useState("");
+
+    const addTask = (e) =>{
+        if(task.trim()){
+            setTasks([...tasks,{text:task,completed:false}]);
+            setTask('');
+        }
+        console.log(tasks);
+    }
+    const toggleComplete = (index) =>{
+      const newTasks = tasks.map((t,i)=>
+        i===index?{...t ,completed:!t.completed}:t);
+      setTasks(newTasks);
+    }
+    const deleteTask = (index)=>{
+      tasks.filter((_,i)=>{
+        setTasks(tasks.filter((_,i) => i !== index));
+      })
+    }
+  return ( 
     <main>
-        <input type="text" />
-        <button>Add</button>
+      <div className='input-area'>
+        <input type="text" value={task} onChange={(e)=> setTask(e.target.value)} placeholder="Enter a task"/>
+        <button  className="add-btn" onClick={(e)=>addTask(e)}>Add</button>
+      </div>
+        <ul>
+        {tasks.map((t, index) => (
+          <li
+            key={index}
+          >
+            <span
+              onClick={() => toggleComplete(index)}
+            >
+              {t.text}
+            </span>
+            <button
+              onClick={() => deleteTask(index)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }

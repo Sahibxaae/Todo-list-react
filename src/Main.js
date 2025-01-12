f17225c9c194546a414a0436be17e2ac040a9513
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-12 19:01:37 +0530

    major changes and bug fixes added media query for smaller devices

--- src/Main.js
+++ src/Main.js
@@ -8,37 +8,45 @@ const Main = () => {
     const handleInputChange = (event)=>{ //textbox input handling
       setNewTask(event.target.value);
     }
-
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter") {
+        addTask();
+      }
+    };
     const addTask = () =>{
+      if(newTask.trim() !== ""){
       setTasks(t=>[...t,newTask]);
       setNewTask("");
     }
+    }
 
     const deleteTask = (index) =>{
-      // tasks.filter((_,i)=>())
+      const updatedTasks = tasks.filter((_,i)=> i !== index);
+      setTasks(updatedTasks);
     }
 
-    const moveTaskUp = (index) =>{
+    // const moveTaskUp = (index) =>{
 
-    }
+    // }
 
-    const moveTaskDown = (index) =>{
+    // const moveTaskDown = (index) =>{
 
-    }
+    // }
   return ( 
     <main>
       <div className='input-area'>
-        <input type="text" placeholder="Enter a task" value={newTask} onChange={(e)=>handleInputChange(e)}/> 
+        <input type="text" placeholder="Enter a task" value={newTask} onChange={(e)=>handleInputChange(e)}  onKeyDown={(e) => handleKeyPress(e)}/> 
         {/* e means event. Here click event is going to happen so we send the parameter to the respective function and we receive through the parameter event and we target the click event value using event.target.value */}
         <button className="add-btn" onClick={addTask}>Add</button>
       </div>
       <ol>
         {tasks.map((task,index)=>(
           <li key={index}>
-            <span className='text'>{task}</span>
-            <button className='delete-btn'onClick={()=>deleteTask()}>delete</button>
-            <button className='up-btn'>up</button>
-            <button className='down-btn'>down</button>
+            <input type="checkbox" id={index} />
+            <label htmlFor={index}>{task}</label>
+            <button className='delete-btn' onClick={()=>deleteTask(index)}>delete</button>
+            {/* <button className='up-btn'>up</button>
+            <button className='down-btn'>down</button> */}
           </li>
         ))}
       </ol>
